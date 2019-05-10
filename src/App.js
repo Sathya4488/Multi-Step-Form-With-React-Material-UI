1ce9e5c17869ed07f80e0a585525be80337d3992
Sathya <sathyamsb91@gmail.com>
2019-05-10 14:48:15 +0530

    ReactJS

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import './App.css';
+
+import UserForm from './Component/UserForm';
+
+
+
+
+class App extends Component{
+  render(){
+    return (
+    <div className="App">
+      <UserForm />
+    </div>
+  );
+
+  }
+}
+export default App;
